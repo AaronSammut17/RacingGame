95a42d0f037b2219406c61796adfbbd909d20540
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2020-05-02 22:14:42 +0200

    Basic car physics implemented

---
+++ RacingGame/Assets/Scripts/CameraStable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    public GameObject PlayerCar;
+    public float CarX;
+    public float CarY;
+    public float CarZ;
+    
+    void Update(){
+        CarX = PlayerCar.transform.eulerAngles.x;
+        CarY = PlayerCar.transform.eulerAngles.y;
+        CarZ = PlayerCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(CarX*-1, CarY, CarZ*-1);
+    }
+}
---
+++ RacingGame/Assets/Scripts/CarController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelCollider WheelFrontLeftCol; //the wheel coliders
+    public WheelCollider WheelFrontRightCol;
+    public WheelCollider WheelRearLeftCol;
+    public WheelCollider WheelRearRightCol;
+
+
+    public GameObject FrontLeftWheel; //the wheel gameObjects
+    public GameObject FrontRightWheel;
+    public GameObject RearLeftWheel;
+    public GameObject RearRightWheel;
+
+    public float topSpeed = 250f; //top speed
+    public float maxTorque = 200f; //maximum torque applied to wheels
+    public float maxSteerAngle = 45f;
+    public float currentSpeed;
+    public float maxBrakeTorque = 2200f;
+
+    private float Forward;
+    private float Turn;
+    private float Brake;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() //better optimised for physics than Update()
+    {
+        Forward = Input.GetAxis("Vertical");
+        Turn = Input.GetAxis("Horizontal");
+        Brake = Input.GetAxis("Jump");
+        
+        WheelFrontLeftCol.steerAngle =  maxSteerAngle * Turn;
+        WheelFrontRightCol.steerAngle =  maxSteerAngle * Turn;
+
+        currentSpeed = 2 * 22/7 * WheelRearLeftCol.radius * WheelRearLeftCol.rpm * 60 / 1000; //formula for calc speed in km/h
+
+        if(currentSpeed < topSpeed){
+            WheelRearLeftCol.motorTorque = maxTorque * Forward; //Rear Wheel Drive movement
+            WheelRearRightCol.motorTorque = maxTorque * Forward;
+            //top speed may not be fully accurate but will attempt to limit the car before set top speed value
+
+            WheelRearLeftCol.brakeTorque = maxBrakeTorque * Brake;
+            WheelRearRightCol.brakeTorque = maxBrakeTorque * Brake;
+            WheelFrontLeftCol.brakeTorque = maxBrakeTorque * Brake;
+            WheelFrontRightCol.brakeTorque = maxBrakeTorque * Brake;
+        }
+    }
+
+    void Update() //called once per frame
+    {
+        Quaternion FLq; //rotation of wheel collider
+        Vector3 FLv; //position of wheel collider
+        WheelFrontLeftCol.GetWorldPose(out FLv, out FLq); //get wheel collider position and rotation
+        FrontLeftWheel.transform.rotation = FLq;
+        FrontLeftWheel.transform.position = FLv;
+        
+        Quaternion FRq; //rotation of wheel collider
+        Vector3 FRv; //position of wheel collider
+        WheelFrontRightCol.GetWorldPose(out FRv, out FRq); //get wheel collider position and rotation
+        FrontRightWheel.transform.rotation = FRq;
+        FrontRightWheel.transform.position = FRv;
+
+        Quaternion RLq; //rotation of wheel collider
+        Vector3 RLv; //position of wheel collider
+        WheelRearLeftCol.GetWorldPose(out RLv, out RLq); //get wheel collider position and rotation
+        RearLeftWheel.transform.rotation = RLq;
+        RearLeftWheel.transform.position = RLv;
+
+        Quaternion RRq; //rotation of wheel collider
+        Vector3 RRv; //position of wheel collider
+        WheelRearRightCol.GetWorldPose(out RRv, out RRq); //get wheel collider position and rotation
+        RearRightWheel.transform.rotation = RRq;
+        RearRightWheel.transform.position = RRv;
+
+    }
+}
+

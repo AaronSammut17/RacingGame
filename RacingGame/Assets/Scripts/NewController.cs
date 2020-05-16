38f7b4f3a0915d45cc66603706f719776a9135ab
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2020-05-16 13:45:38 +0200

    Waypointing + opponent car physics
    - Recreated car controller for opponent AI
    - Scrapped Unity WaypointCircuit & WaypointProgressTracker due in issues regarding ArrayList & wheel collider physics
    - Implemented new waypoint controller using raycast gizmos stored in array
    - Opponent car (and wheel physics) fully functional
    - Current error in pathfinding + calculating distance to next waypoint

---
+++ RacingGame/Assets/Scripts/NewController.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewController : MonoBehaviour
+{
+
+    public Transform wayPoints;
+    public Vector3 centerOfMass;
+
+    public float maxSteerAngle = 45f;
+    public float maxTorque = 50f;
+    public float currentSpeed;
+    public float topSpeed = 150f;
+    public float decelerationSpeed = 10f;
+
+    public WheelCollider WheelFrontLeftCol;
+    public WheelCollider WheelFrontRightCol;
+    public WheelCollider WheelRearLeftCol;
+    public WheelCollider WheelRearRightCol;
+    public GameObject FrontLeftWheel; //the wheel gameObjects
+    public GameObject FrontRightWheel;
+    public GameObject RearLeftWheel;
+    public GameObject RearRightWheel;
+
+    public int currentWaypoint;
+    public float distFromWaypoint;
+
+    public List<Transform> waypoint;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        waypoint = new List<Transform>();
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass;
+        getWaypoint();
+    }
+
+    void getWaypoint()
+    {
+        Transform[] childObjects = wayPoints.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < childObjects.Length; i++)
+        {
+            Transform temp = childObjects[i];
+            if (temp.gameObject.GetInstanceID() != GetInstanceID())
+                waypoint.Add(temp);
+        }
+
+      //  Debug.Log(pathList.Count);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getSteer();
+        Move();
+        
+        Quaternion FLq; //rotation of wheel collider
+        Vector3 FLv; //position of wheel collider
+        WheelFrontLeftCol.GetWorldPose(out FLv, out FLq); //get wheel collider position and rotation
+        FrontLeftWheel.transform.rotation = FLq;
+        FrontLeftWheel.transform.position = FLv;
+
+        Quaternion FRq; //rotation of wheel collider
+        Vector3 FRv; //position of wheel collider
+        WheelFrontRightCol.GetWorldPose(out FRv, out FRq); //get wheel collider position and rotation
+        FrontRightWheel.transform.rotation = FRq;
+        FrontRightWheel.transform.position = FRv;
+
+        Quaternion RLq; //rotation of wheel collider
+        Vector3 RLv; //position of wheel collider
+        WheelRearLeftCol.GetWorldPose(out RLv, out RLq); //get wheel collider position and rotation
+        RearLeftWheel.transform.rotation = RLq;
+        RearLeftWheel.transform.position = RLv;
+
+        Quaternion RRq; //rotation of wheel collider
+        Vector3 RRv; //position of wheel collider
+        WheelRearRightCol.GetWorldPose(out RRv, out RRq); //get wheel collider position and rotation
+        RearRightWheel.transform.rotation = RRq;
+        RearRightWheel.transform.position = RRv;
+    }
+
+    void getSteer()
+    {
+        Vector3 steerVector = transform.InverseTransformPoint(new Vector3(waypoint[currentWaypoint].position.x, transform.position.y, waypoint[currentWaypoint].position.z));
+        float newSteer = maxSteerAngle * (steerVector.x / steerVector.magnitude);
+        WheelFrontLeftCol.steerAngle = newSteer;
+        WheelFrontRightCol.steerAngle = newSteer;
+
+        if (steerVector.magnitude <= distFromWaypoint)
+        {
+            currentWaypoint++;
+        }
+
+        if (currentWaypoint >= waypoint.Count)
+        {
+            currentWaypoint = 0;
+        }
+    }
+
+    
+    void Move()
+    {
+        currentSpeed = 2 * (22 / 7) * WheelRearLeftCol.radius * WheelRearLeftCol.rpm * 60 / 1000;
+        currentSpeed = Mathf.Round(currentSpeed);
+
+        if (currentSpeed <= topSpeed)
+        {
+            WheelRearLeftCol.motorTorque = maxTorque;
+            WheelRearRightCol.motorTorque = maxTorque;
+            WheelRearLeftCol.brakeTorque = 0;
+            WheelRearRightCol.brakeTorque = 0;
+        }
+        else
+        {
+            WheelRearLeftCol.motorTorque = 0;
+            WheelRearRightCol.motorTorque = 0;
+            WheelRearLeftCol.brakeTorque = decelerationSpeed;
+            WheelRearRightCol.brakeTorque = decelerationSpeed;
+        }
+    }
+    
+}
---
+++ RacingGame/Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+    public Color rayColor = Color.white;
+    List<Transform> waypoint;
+
+    void OnDrawGizmos() {
+        Gizmos.color = rayColor;
+        Transform[] waypoint_objs = transform.GetComponentsInChildren<Transform>();
+        waypoint = new List<Transform>();
+
+        foreach(Transform waypoint_obj in waypoint_objs) //foreach
+        {
+            if (waypoint_obj != transform){
+                waypoint.Add(waypoint_obj);
+            }
+        }
+        
+        for (int i = 0; i < waypoint.Count; i++){
+            Vector3 pos = waypoint[i].position;
+            if (i > 0) {
+                Vector3 prev = waypoint[i - 1].position;
+                Gizmos.DrawLine(prev, pos);
+                Gizmos.DrawWireSphere(pos, 2f);
+            }
+        }
+    }
+}

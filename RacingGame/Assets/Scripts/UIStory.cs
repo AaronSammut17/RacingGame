33a11cb9346df2a7ce6ee827547a7700eb5ab1bf
AaronSammut17 <32704546+AaronSammut17@users.noreply.github.com>
2020-05-30 00:27:01 +0200

    added Story script
    added a script that changes the ui and if space is pressed it will  take them to the respective scene

---
+++ RacingGame/Assets/Scripts/UIStory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIStory : MonoBehaviour
+{
+    // variable
+    public static int Story = 0;
+    public GameObject girl1;
+    public GameObject girl2;
+    public GameObject girl3;
+    public GameObject girl4;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Story == 1){
+            girl1.SetActive(true);
+            if(Input.GetKeyDown("space")){
+                SceneManager.LoadScene (2);
+            }
+        }
+        if(Story == 2){
+            girl2.SetActive(true);
+            if(Input.GetKeyDown("space")){
+                SceneManager.LoadScene (3);
+            }
+        }
+        if(Story == 3){
+            girl3.SetActive(true);
+            if(Input.GetKeyDown("space")){
+                SceneManager.LoadScene (5);
+            }
+        }
+        if(Story == 4){
+            girl4.SetActive(true);
+            if(Input.GetKeyDown("space")){
+                SceneManager.LoadScene (4);
+            }
+        }
+    }
+}
